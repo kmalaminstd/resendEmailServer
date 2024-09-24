608dc4f2795a9f64be965a95d71dfebfaf47d74b
kmalaminstd <alaminkhanstd@gmail.com>
2024-09-24 23:57:13 +0600

    first version

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const cors = require("cors")
+const {Resend} = require("resend")
+
+const app = express()
+
+const resend = new Resend('re_123456789')
+
+app.use(cors())
+app.use(express.json())
+
+app.post('/send-email', async (req, res)=>{
+    const {fullname, email, message} = req.body
+
+    try{
+
+        const {data, error} = await resend.emails.send({
+            from: `Acme <${email}>`,
+            to: ['alaminkhanstd@gmail.com'],
+            subject: 'Admin contact from portfolio',
+            html: `<p>${message}</p>`,
+        })
+
+        if(error){
+            return res.status(500).json({error: error.message})
+        }
+
+        res.status(200).json({data})
+
+    }catch(err){
+        res.status(500).json({error: err.message})
+    }
+})
+
+
+app.listen(5000, ()=>{
+    console.log('Server running on the port 5000');
+})
